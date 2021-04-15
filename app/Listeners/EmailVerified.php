3154b7498be4346b6f8ddc7ea37f86cc7e1ff2cd
liuyang87075853 <liuyang87075853@163.com>
2021-04-15 08:16:23 +0000

    认证后的消息提醒

---
+++ app/Listeners/EmailVerified.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Auth\Events\Verified;
+
+class EmailVerified
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  Verified  $event
+     * @return void
+     */
+    public function handle(Verified $event)
+    {
+        //会话里闪存认证成功后的消息提醒
+        session()->flash('success', '邮箱验证成功^_^');
+    }
+}
